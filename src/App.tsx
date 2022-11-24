c98725de7a3255b5d32c98a0fcb576689da8d044
jayantwaldia <imjayantwaldia@gmail.com>
2022-11-25 00:11:56 +0530

    events in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Button from "./components/Button";
 import { Greet } from "./components/Greet";
 import Heading from "./components/Heading";
 import { Oscar } from "./components/Oscar";
@@ -37,6 +38,7 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to the Ice-Cream parlour</Heading>
       </Oscar>
+      <Button handleClick={(event, id) => console.log('Button is clicked', event)} />
     </div>
   );
 }
